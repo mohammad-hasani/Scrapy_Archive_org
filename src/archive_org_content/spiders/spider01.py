f861267d9563a85e7c41bb97a9ac8748e02d1440
sina <mohammad.h4s4ni@gmail.com>
2020-01-19 04:25:50 +0330

    3

--- src/archive_org_content/spiders/spider01.py
+++ src/archive_org_content/spiders/spider01.py
@@ -17,9 +17,9 @@ class Spider01Spider(scrapy.Spider):
             # ('Geschichte', 'German'),
             # ('कहानी', 'Hindi'),
             # ('قصة', 'Arabic'),
-            # ('物語', 'Japanese'),
-            # ('داستان', 'Persian'),
-            ('故事', 'Chinese')
+            ('物語', 'Japanese'),
+            ('داستان', 'Persian')
+            # ('故事', 'Chinese')
         ]
         base_url = 'https://archive.org/search.php?query={0}&and[]=languageSorter%3A%22{1}%22&and[]=mediatype%3A%22texts%22&and[]=loans__status__status%3A%22-1%22'
         for search in search_words:
